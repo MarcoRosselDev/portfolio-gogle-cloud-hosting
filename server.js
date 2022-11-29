cede38ad5f13e12748936865d7f2038bb1a08000
marco rossel <97572795+MarcoRosselDev@users.noreply.github.com>
2022-11-29 03:50:15 -0300

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('New connection');
     
-    res.end('Hello moko')
+    res.end('Hello moko and now more')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, ()=> console.log('Listening')
